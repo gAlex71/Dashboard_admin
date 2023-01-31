23e2e75ea33636eff27a54bb6289b770bdf9f561
gAlex71 <alexvladimirovich71@mail.ru>
2023-01-31 23:51:52 +0300

    calendar and FAQ

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Dashboard/>} />
               <Route path="/team" element={<Team/>} />
-              <Route path="/calendar" element={<Calendar/>} />
+              {/* <Route path="/calendar" element={<Calendar/>} /> */}
               <Route path="/invoices" element={<Invoices/>} />
               <Route path="/contacts" element={<Contacts/>} />
               {/* <Route path="/bar" element={<Bar/>} /> */}
               <Route path="/form" element={<Form/>} />
               {/* <Route path="/line" element={<Line/>} />
-              <Route path="/pie" element={<Pie/>} />
+              <Route path="/pie" element={<Pie/>} /> */}
               <Route path="/faq" element={<Faq/>} />
-              <Route path="/geography" element={<Geography/>} /> */}
+              {/* <Route path="/geography" element={<Geography/>} /> */}
             </Routes>
           </main>
         </div>
